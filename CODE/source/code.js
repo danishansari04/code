c5f23c6ed7156924390679397b58eb4e6ad7e697
Danish Ansari <117536349+danishansari04@users.noreply.github.com>
2023-01-09 02:14:47 -0800

    gfy

---
+++ CODE/source/code.js
@@ -0,0 +1,17 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="danishansari"&& password=="123456")
+{
+    alert("login succesfully");
+    window.open("https://drive.google.com/file/d/1fvP7GG6XJcFUeBZ3nIVn9aylFoV4PuWN/view?usp=share_link")
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
